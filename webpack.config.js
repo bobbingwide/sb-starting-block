e6543caf06646168b82951a967da8eb7a8b4cfa4
Herb Miller <herb@bobbingwide.com>
2022-03-06 23:59:52 +0000

    Add webpack.config.js for multiple entry points #3

---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
+module.exports = {
+	...defaultConfig,
+	entry: {
+		'sb-starting-block': './src/starting-block',
+		'second-block': './src/second-block'
+	},
+};

107b513543922a177c13eb985199a0d192137d21
Herb Miller <herb@bobbingwide.com>
2022-03-07 00:13:10 +0000

    Update server logic for multiple entry points #3

--- sb-starting-block.php
+++ sb-starting-block.php
@@ -4,7 +4,7 @@
  * Description:       Starting point for a single block server side rendered block
  * Requires at least: 5.7
  * Requires PHP:      7.3
- * Version:           0.0.1
+ * Version:           0.1.0
  * Author:            bobbingwide
  * License:           GPLv3
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
@@ -14,7 +14,7 @@
  */
 
 /**
- * Registers the block using the metadata loaded from the `block.json` file.
+ * Registers the blocks using the metadata loaded from their `block.json` files.
  * Behind the scenes, it registers also all assets so they can be enqueued
  * through the block editor in the corresponding context.
  *
@@ -23,26 +23,41 @@
 function oik_sb_sb_starting_block_block_init() {
 	load_plugin_textdomain( 'sb-starting-block', false, 'sb-starting-block/languages' );
 	$args = [ 'render_callback' => 'oik_sb_sb_starting_block_dynamic_block'];
-	register_block_type_from_metadata( __DIR__ . '/src/starting-block', $args );
-	register_block_type_from_metadata( __DIR__ . '/src/second-block' );
-
+	$registered = register_block_type_from_metadata( __DIR__ . '/src/starting-block', $args );
+	//bw_trace2( $registered, "registered");
+	//print_r( $registered);
+	$registered = register_block_type_from_metadata( __DIR__ . '/src/second-block' );
+	//print_r( $registered);
+	//bw_trace2( $registered, "registered");
 
 	/**
-	 * Localise the script by loading the required strings for the build/index.js file
+	 * Localise the script by loading the required strings for the build/entry-point.js file
 	 * from the locale specific .json file in the languages folder.
-	 * oik-sb/sb-starting-block
+	 *
+	 * The script handle for oik-sb/sb-starting-block is oik-sb-sb-starting-block-editor-script
+	 * For oik-sb/second-block it's oik-sb-second-block-editor-script
+	 *
+	 * The filter function can be registered either before or after the wp_set_script_translations calls.
+	 *
+	 * The load_script_textdomain_relative_path filter gets called for every script that's been enqueued
+	 * but we need to call wp_set_script_translations for each block's editor script in order to tell it which folder
+	 * to look in.
 	 */
-	$ok = wp_set_script_translations( 'oik-sb-sb-starting-block-editor-script', 'sb-starting-block' , __DIR__ .'/languages' );
-	//bw_trace2( $ok, "OK?");
-	add_filter( 'load_script_textdomain_relative_path', 'oik_sb_sb_starting_block_load_script_textdomain_relative_path', 10, 2);
+	$ok=wp_set_script_translations( 'oik-sb-sb-starting-block-editor-script', 'sb-starting-block', __DIR__ . '/languages' );
+	$ok=wp_set_script_translations( 'oik-sb-second-block-editor-script', 'sb-starting-block', __DIR__ . '/languages' );
 
+	//bw_trace2( $ok, "OK?", false );
+	//	$wp_scripts=wp_scripts();
+	//print_r( $wp_scripts );
+
+	add_filter( 'load_script_textdomain_relative_path', 'oik_sb_sb_starting_block_load_script_textdomain_relative_path', 10, 2 );
 }
 
 /**
  * Filters $relative so that md5's match what's expected.
  *
- * Depending on how it was built the `build/index.js` may be preceded by `./` or `src/block-name/../../`.
- * In either of these situations we want the $relative value to be returned as `build/index.js`.
+ * Depending on how it was built the `build/entry-point.js` may be preceded by `./` or `src/block-name/../../`.
+ * In either of these situations we want the $relative value to be returned as `build/entry-point.js`.
  * This then produces the correct md5 value and the .json file is found.
  *
  * @param $relative
@@ -51,10 +66,11 @@ function oik_sb_sb_starting_block_block_init() {
  * @return mixed
  */
 function oik_sb_sb_starting_block_load_script_textdomain_relative_path( $relative, $src ) {
-	if ( false !== strrpos( $relative, './build/index.js' )) {
-		$relative = 'build/index.js';
+	//bw_trace2();
+	if ( false !== strpos( $src, '/sb-starting-block/src/' )) {
+		$relative = 'build/' . basename( $relative );
 	}
-	//bw_trace2( $relative, "relative");
+	//bw_trace2( $relative, "relative", false);
 	return $relative;
 }
 
@@ -78,7 +94,7 @@ function oik_sb_sb_starting_block_dynamic_block( $attributes ) {
 	$wrapper_attributes = get_block_wrapper_attributes( array( 'class' => $classes ) );
 	$localised_time = date_i18n( get_option( 'time_format'));
 	/* translators: %s: time in user's preferred format */
-	$content = sprintf( __( 'Starting block rendered at %s', 'sb-starting-block'), $localised_time );
+	$content = sprintf( __( 'Starting block rendered at %s on %s', 'sb-starting-block'), $localised_time, date( 'Y/m/d') );
 	$html = sprintf( '<div %1$s>%2$s</div>', $wrapper_attributes, $content );
 	return $html;
 }

c6daf05e71a3acd0f0d121de199d0dcb77f15132
Herb Miller <herb@bobbingwide.com>
2022-03-07 09:26:36 +0000

    Comment that this file is no longer used #3

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-// This file is no longer needed.
-// Each block is built separately.
+// This index.js file is no longer needed since each block is built separately
+// with a separate entry point for each block.
 // See webpack.config.js
 
 import './starting-block';

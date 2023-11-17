6be3dff4fb01233bf540d63e9f7057850d6330e5
Herb Miller <herb@bobbingwide.com>
2023-11-17 14:38:20 +0000

    Add PHPUnit tests for PHP 8.1 and PHP 8.2 #4

---
+++ tests/test-a-load-php.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * @package sb-starting-block
+ * @copyright (C) Copyright Bobbing Wide 2023
+ *
+ * Unit tests to load all the files for PHP 8.2, except batch ones
+ */
+
+class Tests_load_libs extends BW_UnitTestCase
+{
+
+	/**
+	 * set up logic
+	 *
+	 * - ensure any database updates are rolled back
+	 * - we need oik-googlemap to load the functions we're testing
+	 */
+	function setUp(): void
+	{
+		parent::setUp();
+
+	}
+
+	/**
+	 * Test that the plugin is loaded
+	 */
+	function test_load_plugin() {
+		oik_require( 'sb-starting-block.php', 'sb-starting-block');
+		$this->assertTrue( true );
+	}
+
+}
